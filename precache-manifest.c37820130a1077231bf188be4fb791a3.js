a8c0b4a2014018542dbf63791de313922ee4225f
boostup <boostup@gmail.com>
2020-08-16 22:25:06 +0200

    Updates

---
+++ precache-manifest.c37820130a1077231bf188be4fb791a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7004d6ffbe4f4a52b422424e7ccdf55a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2b7bc2f56c0e2dc01c2a",
+    "url": "./static/css/main.9a1851d0.chunk.css"
+  },
+  {
+    "revision": "6b799dd8d0b15abda554",
+    "url": "./static/js/2.37ed7961.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.37ed7961.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b7bc2f56c0e2dc01c2a",
+    "url": "./static/js/main.4cb18315.chunk.js"
+  },
+  {
+    "revision": "f601f028fc1d52d54436",
+    "url": "./static/js/runtime-main.b4bb5a65.js"
+  }
+]);
