2024254d4cabcb01f0425071db8d30b811c4426a
boostup <boostup@gmail.com>
2020-10-04 08:07:56 +0200

    new LocalStorageDemo

--- example/src/App.js
+++ example/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import LocalStorageDemo from './demoComponents/LocalStorageDemo'
 import TogglesDemo from './demoComponents/TogglesDemo'
 import SWMoviesDemo from './demoComponents/SWMoviesDemo'
 import InputTextDemo from './demoComponents/InputTextDemo'
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className='App'>
       <h1>React Hooks Demo</h1>
+      <LocalStorageDemo />
       <TogglesDemo />
       <SWMoviesDemo />
       <InputTextDemo />
---
+++ example/src/demoComponents/LocalStorageDemo.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { useLocalStorageState } from 'react-custom-hooks-collection'
+
+export default () => {
+  const [currentDate, setCurrentDate] = useLocalStorageState(
+    'lastSetDate',
+    new Date()
+  )
+
+  const handleClick = (e) => setCurrentDate(new Date())
+
+  const formatDate = (dateString) => new Date(dateString).toLocaleString()
+
+  return (
+    <div>
+      <h3>Last save</h3>
+      <p>{formatDate(currentDate)}</p>
+      <button onClick={handleClick}>Save Date</button>
+      <p className='note'>
+        By pressing this button, the current date and time will be saved to
+        localStorage. You can close or refresh this browser tab or window, and
+        the date and time previously saved will be displayed above the button.
+      </p>
+    </div>
+  )
+}
