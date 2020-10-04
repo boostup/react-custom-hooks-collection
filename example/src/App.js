83e3491a46e026ffcdc490c7aad786d9e7f723a6
boostup <boostup@gmail.com>
2020-10-04 08:38:57 +0200

    new demo : useLocalStorageReducer

--- example/src/App.js
+++ example/src/App.js
@@ -1,18 +1,20 @@
 import React from 'react'
-import LocalStorageDemo from './demoComponents/LocalStorageDemo'
 import TogglesDemo from './demoComponents/TogglesDemo'
 import SWMoviesDemo from './demoComponents/SWMoviesDemo'
 import InputTextDemo from './demoComponents/InputTextDemo'
+import LocalStorageDemo from './demoComponents/LocalStorageDemo'
+import LocalStorageReducerDemo from './demoComponents/LocalStorageReducerDemo'
 import './styles.css'
 
 function App() {
   return (
     <div className='App'>
       <h1>React Hooks Demo</h1>
-      <LocalStorageDemo />
       <TogglesDemo />
       <SWMoviesDemo />
       <InputTextDemo />
+      <LocalStorageDemo />
+      <LocalStorageReducerDemo />
     </div>
   )
 }
